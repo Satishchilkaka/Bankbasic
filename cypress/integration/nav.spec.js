6295414bd416c4051c1e2ccca367a36527e3d018
satish <satishchilkaka8@gmail.com>
2020-12-20 21:54:33 -0500

    add simple spec

---
+++ cypress/integration/nav.spec.js
@@ -0,0 +1,11 @@
+
+describe('basic bank navigation', () => {
+    beforeEach(function() {
+            cy.visit('/');
+          });
+
+          it('it should navigate to', () =>{
+
+          })
+
+})
